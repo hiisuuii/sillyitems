ceec70cd9c92ca7c3130d70c32486e008859fd9e
hiisuuii <dogglez7777@gmail.com>
2024-01-31 04:21:19 -0700

    fix old modid

--- src/client/java/hisui/sillyitems/SillyItemsClient.java
+++ src/client/java/hisui/sillyitems/SillyItemsClient.java
@@ -2,7 +2,7 @@ package hisui.sillyitems;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class AzidoazideazideClient implements ClientModInitializer {
+public class SillyItemsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
