f9a42860ec172b74a4ca2b34887f8449bd63b609
hiisuuii <dogglez7777@gmail.com>
2024-02-01 04:33:10 -0700

    Unfuck everything

--- src/main/java/hisui/sillyitems/SillyItems.java
+++ src/main/java/hisui/sillyitems/SillyItems.java
@@ -1,10 +1,12 @@
 package hisui.sillyitems;
 
-import hisui.item.AzidoazideAzideItem;
+import hisui.sillyitems.item.AzidoazideAzideItem;
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.StewItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,20 +16,25 @@ import org.slf4j.LoggerFactory;
 public class SillyItems implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors
+	// That way, it's clear which mod wrote info, warnings, and errors.
 
 	public static final String MODID = "sillyitems";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-	public static final Item AZIDOAZIDEAZIDE = new AzidoazideAzideItem(new FabricItemSettings());
+	public static final Item AZIDOAZIDEAZIDE = registerItem(new AzidoazideAzideItem(new Item.Settings()), "azidoazideazide");
+	public static final Item BLINDNESS_SOUP = registerItem(new StewItem(new Item.Settings().food(
+			new FoodComponent.Builder().hunger(6).saturationModifier(0.6f).statusEffect(
+					new StatusEffectInstance(StatusEffects.BLINDNESS, 24000), 1).alwaysEdible().build()
+			)
+	), "blindness_stew");
+
+	public static Item registerItem(Item item, String name){
+		return Registry.register(Registries.ITEM, new Identifier(MODID, name), item);
+	}
 
 	@Override
 	public void onInitialize() {
 
-		Registry.register(Registries.ITEM, new Identifier(MODID,"sillyitems"), AZIDOAZIDEAZIDE);
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/hisui/sillyitems/SillyItemsDataGenerator.java
@@ -0,0 +1,90 @@
+package hisui.sillyitems;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+
+import java.util.function.Consumer;
+
+public class SillyItemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModModelGenerator::new);
+		pack.addProvider(ModRecipeGenerator::new);
+		pack.addProvider(ModLanguageGenerator::new);
+	}
+
+	private static class ModModelGenerator extends FabricModelProvider {
+		private ModModelGenerator(FabricDataOutput generator){
+			super(generator);
+		}
+
+		@Override
+		public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+
+		}
+
+		@Override
+		public void generateItemModels(ItemModelGenerator itemModelGenerator){
+
+			itemModelGenerator.register(SillyItems.AZIDOAZIDEAZIDE, Models.GENERATED);
+		}
+	}
+
+	private static class ModRecipeGenerator extends FabricRecipeProvider {
+
+		public ModRecipeGenerator(FabricDataOutput output) {
+			super(output);
+		}
+
+		@Override
+		public void generate(Consumer<RecipeJsonProvider> exporter) {
+			ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, SillyItems.AZIDOAZIDEAZIDE).input(Items.DIRT)
+					.criterion(FabricRecipeProvider.hasItem(Items.DIRT),
+							FabricRecipeProvider.conditionsFromItem(Items.DIRT))
+					.criterion(FabricRecipeProvider.hasItem(SillyItems.AZIDOAZIDEAZIDE),
+							FabricRecipeProvider.conditionsFromItem(SillyItems.AZIDOAZIDEAZIDE)).offerTo(exporter);
+
+			ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, SillyItems.BLINDNESS_SOUP).input(Items.BOWL)
+					.input(Items.BROWN_MUSHROOM).input(Items.RED_MUSHROOM).input(Items.SPIDER_EYE)
+					.input(Items.NETHER_WART).input(Items.FEATHER)
+					.criterion(FabricRecipeProvider.hasItem(Items.BOWL),
+							FabricRecipeProvider.conditionsFromItem(Items.BOWL))
+					.criterion(FabricRecipeProvider.hasItem(Items.NETHER_WART),
+							FabricRecipeProvider.conditionsFromItem(Items.NETHER_WART))
+					.criterion(FabricRecipeProvider.hasItem(Items.RED_MUSHROOM),
+							FabricRecipeProvider.conditionsFromItem(Items.RED_MUSHROOM))
+					.criterion(FabricRecipeProvider.hasItem(Items.BROWN_MUSHROOM),
+							FabricRecipeProvider.conditionsFromItem(Items.BROWN_MUSHROOM))
+					.criterion(FabricRecipeProvider.hasItem(Items.SPIDER_EYE),
+							FabricRecipeProvider.conditionsFromItem(Items.SPIDER_EYE))
+					.criterion(FabricRecipeProvider.hasItem(Items.FEATHER),
+							FabricRecipeProvider.conditionsFromItem(Items.FEATHER))
+					.criterion(FabricRecipeProvider.hasItem(SillyItems.BLINDNESS_SOUP),
+							FabricRecipeProvider.conditionsFromItem(SillyItems.BLINDNESS_SOUP)).offerTo(exporter);
+		}
+	}
+
+	private static class ModLanguageGenerator extends FabricLanguageProvider {
+		protected ModLanguageGenerator(FabricDataOutput dataOutput) {
+			super(dataOutput);
+		}
+
+		@Override
+		public void generateTranslations(TranslationBuilder tb) {
+			tb.add(SillyItems.AZIDOAZIDEAZIDE, "Azidoazide Azide");
+			tb.add(SillyItems.BLINDNESS_SOUP, "1-Day Blindness Stew");
+		}
+	}
+}
